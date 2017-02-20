b98da1f605775bbc5c36f389cebad41657c0b66a
kiwamunet <kiwamunet@gmail.com>
2017-02-20 10:18:39 +0900

    build test

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ THRESHOLD_FAIL=${WERCKER_GOLINT_THRESHOLD_FAIL-10}
 
 go get -v -u github.com/alecthomas/gometalinter
 GOMETALINTER=`which gometalinter`
-${GOMETALINTER} --install --update
+`which gometalinter` --install --update
 
 LINTLINES=$(${GOMETALINTER} ./... | tee gometalinter_results.txt | wc -l | tr -d " ")
 
@@ -16,3 +16,6 @@ else
 fi
 
 cat gometalinter_results.txt
+if [ "$LINTLINES" -ge "${THRESHOLD_FAIL}" ]; then echo "Time to tidy up: $LINTLINES gometalinter warnings." > "$WERCKER_REPORT_MESSAGE_FILE"; fail "Time to tidy up."; fi
+if [ "$LINTLINES" -ge "${THRESHOLD_WARN}" ]; then echo "You should be tidying soon: $LINTLINES gometalinter warnings." > "$WERCKER_REPORT_MESSAGE_FILE"; warn "You should be tidying soon."; fi
+if [ "$LINTLINES" -gt 0 ]; then echo "You are fairly tidy: $LINTLINES gometalinter warnings." > "$WERCKER_REPORT_MESSAGE_FILE"; fi
